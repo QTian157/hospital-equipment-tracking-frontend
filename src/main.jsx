04e8f540f852edf2f066befd5d7a1345d126d637
QTian157 <qtian31@gmail.com>
2026-08-17 11:26:04 -0500

    uninstall npm uninstall @fortawesome/fontawesome-free and use fontawsome font kit in main.js

--- src/main.jsx
+++ src/main.jsx
@@ -4,8 +4,6 @@ import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router'
 
-import '@fortawesome/fontawesome-free/css/all.min.css';
-
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
