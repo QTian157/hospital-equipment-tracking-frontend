4da62750aa2773fd4b96b0be4fbbc4ed062f901b
QTian157 <qtian31@gmail.com>
2026-08-17 16:48:07 -0500

    have the table template for equipmentListPage

--- src/components/pages/equipment/EquipmentListPage.jsx
+++ src/components/pages/equipment/EquipmentListPage.jsx
@@ -18,7 +18,29 @@ const EquipmentListPage = ({equipmentList, isLoading, equipListError}) =>{
             <ul>
                 {equipmentList.map((e)=> <li key={e.id}>{e.name}</li>)}
             </ul>
+            <table>
+                <thead>
+                    <tr>
+                        <th>NAME</th>
+                        <th>ASSET TAG</th>
+                        <th>STATUS</th>
+                        <th>DEPARTMENT</th>
+                        <th>ROOM</th>
+                        <th>ACTION</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>NAME</td>
+                        <td>ASSET TAG</td>
+                        <td>STATUS</td>
+                        <td>DEPARTMENT</td>
+                        <td>ROOM</td>
+                        <td>ACTION</td>
+                    </tr>
 
+                </tbody>
+            </table>
         </div>
     )
 }
