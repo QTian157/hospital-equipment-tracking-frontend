5fa4c98593fe9b6fa9f5814f153bbe0ccf65e0b5
QTian157 <qtian31@gmail.com>
2026-08-26 21:42:08 -0500

    globle CSS

--- src/components/pages/AboutPage.jsx
+++ src/components/pages/AboutPage.jsx
@@ -1,28 +1,26 @@
 const AboutPage = () =>{
     return (
-        <main>
-            <div className="main-content">
-                <h1>
-                    <strong>About</strong>
-                </h1>
-                <p>
-                    About Hospital Equipment Tracking
-                </p>
-                <p>
-                    Hospital Equipment Tracking is designed to help hospital staff find, view, and manage medical equipment efficiently.
-                </p>
-                <p>
-                    <strong>
-                        KEY FEATURE
-                    </strong>
-                </p>
-                <ul>
-                    <li>Equipment Tracking</li>
-                    <li>Quick Search </li>
-                    <li>Equipment Management</li>
-                </ul>
-            </div>
-        </main>
+        <div className="main-content">
+            <h1>
+                <strong>About</strong>
+            </h1>
+            <p>
+                About Hospital Equipment Tracking
+            </p>
+            <p>
+                Hospital Equipment Tracking is designed to help hospital staff find, view, and manage medical equipment efficiently.
+            </p>
+            <p>
+                <strong>
+                    KEY FEATURE
+                </strong>
+            </p>
+            <ul>
+                <li>Equipment Tracking</li>
+                <li>Quick Search </li>
+                <li>Equipment Management</li>
+            </ul>
+        </div>
     )
 }
 
